6269e6d80d04bc725f37084bf6f82939ae2d1511
shikha100067 <sonishikha972@gmail.com>
2024-07-09 12:17:16 +0530

    Add files via upload

---
+++ py.list.py
@@ -0,0 +1,26 @@
+print("SHIVANI PRAJAPATI")
+# different operation perform on a list 
+list= [93,4,7,8,9,12,34]
+print(list)
+# check the data type
+print (type(list))
+# popping the first element
+list.pop(0)
+print(list)
+# appending new value into the list
+list.append(46)
+print(list)
+# changing the element at 0 index
+list[0]=24
+print(list)
+# reversing element of a list
+list.reverse()
+print(list)
+# different operations perform on a tuple
+tup1=(1,4,'ant','true',7,4,3.14)
+print(tup1)
+#check the data type of the object
+print(type(tup1))
+# how to extract individual element from a tuple
+print(tup1[-1])
+print(tup1[2])
